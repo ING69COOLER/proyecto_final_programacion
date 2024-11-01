745c12f99b4e00f9b30239a351c5362c035c0944
ING69COOLER <159482318+ING69COOLER@users.noreply.github.com>
2024-11-01 17:56:00 -0500

    sadf

--- src/main/java/co/edu/uniquindio/poo/dataBase/IBuildBaseDeDatos.java
+++ src/main/java/co/edu/uniquindio/poo/dataBase/IBuildBaseDeDatos.java
@@ -1,5 +1,8 @@
 package co.edu.uniquindio.poo.dataBase;
 
+// posibles soluciones
+//1- crear crud de cada tabla
+//2- usar composite para manejarlos todos
 public interface IBuildBaseDeDatos {
     void crearTablaEvento();
     void crearTablaPersonas();
